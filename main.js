8ea760ec4e321f05d5cc5c1e59e892f38b5523b9
Leila Sosa <67832903+leisosag@users.noreply.github.com>
2020-08-25 22:01:41 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburguer.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
+
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0)
-})
+});
