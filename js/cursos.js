a8a1425eff662484fcdd1598bf612d8eff26e64c
Lei Sosa <leisosa93@gmail.com>
2020-10-28 10:40:21 -0300

    cambios en diseño

--- js/cursos.js
+++ js/cursos.js
@@ -3,7 +3,7 @@ const contenedorCursos = document.getElementById('contenedor-cursos');
 class Cursos {
     async traerCursos() {
         try {
-            let resultado = await fetch('cursos.json');
+            let resultado = await fetch('js/cursos.json');
             let infoCursos = await resultado.json();
             let cursos = infoCursos.cursos;
             cursos = cursos.map(curso => {
@@ -22,13 +22,15 @@ class UI {
         let resultado = '';
         cursos.forEach(curso => {
             resultado += `
-            <div class="card card-cursos ml-2 mr-2 mb-4 mt-5" style="width: 20rem;">
-                <img src="${curso.img}" class="card-img-top" alt="${curso.titulo}">
-                <div class="card-body text-center">
-                    <h5 class="card-title">${curso.titulo}</h5>
-                    <p class="card-text">$${curso.precio}</p>
-                    <a href="${curso.href}" class="btn mt-4">Ver más</a>
-                    <a href="${curso.id}" class="btn btn-reservar mt-4" id="${curso.id}">Comprar</a>
+            <div class="col-lg-3 col-sm-6 col-xs-12">
+                <div class="card card-cursos bg-light ml-2 mr-2 mb-4 mt-5">
+                    <img src="${curso.img}" class="card-img-top" alt="${curso.titulo}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${curso.titulo}</h5>
+                        <p class="card-text">$${curso.precio}</p>
+                        <a href="${curso.href}" class="btn mt-4">Ver más</a>
+                        <a href="${curso.id}" class="btn btn-lleno mt-4" id="${curso.id}">Comprar</a>
+                    </div>
                 </div>
             </div>
             `;
